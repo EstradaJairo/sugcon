203198bb0e85c2bd7e9c36f3042c14ac3f7bded0
EstradaJairo <destrada@jairosolutions.com>
2024-01-15 11:49:51 +0800

    Fix Build Error

--- actions/data-content.tsx
+++ actions/data-content.tsx
@@ -1,87 +1,87 @@
-// import {
-//   Navbar,
-//   Banner,
-//   JoinCommunity,
-//   Sugcon,
-//   SugconCommunity,
-//   Events,
-//   Footer,
-//   Navigation,
-// } from "@/types";
+import {
+  Navbar,
+  Banner,
+  JoinCommunity,
+  Sugcon,
+  SugconCommunity,
+  Events,
+  Footer,
+  Navigation,
+} from "@/types";
 
-// const navbarURL = `${process.env.NEXT_PUBLIC_API_URL}/navbar`;
-// const navigationURL = `${process.env.NEXT_PUBLIC_API_URL}/navigation`;
-// const bannerURL = `${process.env.NEXT_PUBLIC_API_URL}/banner`;
-// const joinCommunityURL = `${process.env.NEXT_PUBLIC_API_URL}/join-community`;
-// const sugconURL = `${process.env.NEXT_PUBLIC_API_URL}/sugcon`;
-// const sugconCommunityURL = `${process.env.NEXT_PUBLIC_API_URL}/sugcon-community`;
-// const eventsURL = `${process.env.NEXT_PUBLIC_API_URL}/events`;
-// const footerURL = `${process.env.NEXT_PUBLIC_API_URL}/footer`;
+const navbarURL = `${process.env.NEXT_PUBLIC_API_URL}/navbar`;
+const navigationURL = `${process.env.NEXT_PUBLIC_API_URL}/navigation`;
+const bannerURL = `${process.env.NEXT_PUBLIC_API_URL}/banner`;
+const joinCommunityURL = `${process.env.NEXT_PUBLIC_API_URL}/join-community`;
+const sugconURL = `${process.env.NEXT_PUBLIC_API_URL}/sugcon`;
+const sugconCommunityURL = `${process.env.NEXT_PUBLIC_API_URL}/sugcon-community`;
+const eventsURL = `${process.env.NEXT_PUBLIC_API_URL}/events`;
+const footerURL = `${process.env.NEXT_PUBLIC_API_URL}/footer`;
 
-// export const getNavbarDataContent = async (): Promise<Navbar[]> => {
-//   const res = await fetch(`${navbarURL}`, {
-//     next: { revalidate: 3600 },
-//   });
-//   const data = await res.json();
-//   return data;
-// };
+export const getNavbarDataContent = async (): Promise<Navbar[]> => {
+  const res = await fetch(`${navbarURL}`, {
+    next: { revalidate: 3600 },
+  });
+  const data = await res.json();
+  return data;
+};
 
-// export const getNavigationDataContent = async (): Promise<Navigation[]> => {
-//   const res = await fetch(`${navigationURL}`, {
-//     next: { revalidate: 3600 },
-//   });
-//   const data = await res.json();
-//   return data;
-// };
+export const getNavigationDataContent = async (): Promise<Navigation[]> => {
+  const res = await fetch(`${navigationURL}`, {
+    next: { revalidate: 3600 },
+  });
+  const data = await res.json();
+  return data;
+};
 
-// export const getBannerDataContent = async (): Promise<Banner[]> => {
-//   const res = await fetch(`${bannerURL}`, {
-//     next: { revalidate: 3600 },
-//   });
-//   const data = await res.json();
-//   return data;
-// };
+export const getBannerDataContent = async (): Promise<Banner[]> => {
+  const res = await fetch(`${bannerURL}`, {
+    next: { revalidate: 3600 },
+  });
+  const data = await res.json();
+  return data;
+};
 
-// export const getJoinCommunityDataContent = async (): Promise<
-//   JoinCommunity[]
-// > => {
-//   const res = await fetch(`${joinCommunityURL}`, {
-//     next: { revalidate: 3600 },
-//   });
-//   const data = await res.json();
-//   return data;
-// };
+export const getJoinCommunityDataContent = async (): Promise<
+  JoinCommunity[]
+> => {
+  const res = await fetch(`${joinCommunityURL}`, {
+    next: { revalidate: 3600 },
+  });
+  const data = await res.json();
+  return data;
+};
 
-// export const getSugconDataContent = async (): Promise<Sugcon[]> => {
-//   const res = await fetch(`${sugconURL}`, {
-//     next: { revalidate: 3600 },
-//   });
-//   const data = await res.json();
-//   return data;
-// };
+export const getSugconDataContent = async (): Promise<Sugcon[]> => {
+  const res = await fetch(`${sugconURL}`, {
+    next: { revalidate: 3600 },
+  });
+  const data = await res.json();
+  return data;
+};
 
-// export const getSugconCommunityDataContent = async (): Promise<
-//   SugconCommunity[]
-// > => {
-//   const res = await fetch(`${sugconCommunityURL}`, {
-//     next: { revalidate: 3600 },
-//   });
-//   const data = await res.json();
-//   return data;
-// };
+export const getSugconCommunityDataContent = async (): Promise<
+  SugconCommunity[]
+> => {
+  const res = await fetch(`${sugconCommunityURL}`, {
+    next: { revalidate: 3600 },
+  });
+  const data = await res.json();
+  return data;
+};
 
-// export const getEventsDataContent = async (): Promise<Events[]> => {
-//   const res = await fetch(`${eventsURL}`, {
-//     next: { revalidate: 3600 },
-//   });
-//   const data = await res.json();
-//   return data;
-// };
+export const getEventsDataContent = async (): Promise<Events[]> => {
+  const res = await fetch(`${eventsURL}`, {
+    next: { revalidate: 3600 },
+  });
+  const data = await res.json();
+  return data;
+};
 
-// export const getFooterDataContent = async (): Promise<Footer[]> => {
-//   const res = await fetch(`${footerURL}`, {
-//     next: { revalidate: 3600 },
-//   });
-//   const data = await res.json();
-//   return data;
-// };
+export const getFooterDataContent = async (): Promise<Footer[]> => {
+  const res = await fetch(`${footerURL}`, {
+    next: { revalidate: 3600 },
+  });
+  const data = await res.json();
+  return data;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-// import {
-//   getBannerDataContent,
-//   getEventsDataContent,
-//   getJoinCommunityDataContent,
-//   getNavbarDataContent,
-//   getNavigationDataContent,
-//   getSugconCommunityDataContent,
-//   getSugconDataContent,
-// } from "@/actions/data-content";
+import {
+  getBannerDataContent,
+  getEventsDataContent,
+  getJoinCommunityDataContent,
+  getNavbarDataContent,
+  getNavigationDataContent,
+  getSugconCommunityDataContent,
+  getSugconDataContent,
+} from "@/actions/data-content";
 import Banner from "@/components/banner";
 import Events from "@/components/events";
 import JoinCommunity from "@/components/join-community";
@@ -17,17 +17,17 @@ import SugconCommunity from "@/components/sugcon-community";
 import YourComponent from "@/components/test";
 
 export default async function Home() {
-  // const navbarDataContent = await getNavbarDataContent();
-  // const navigationDataContent = await getNavigationDataContent();
-  // const bannerDataContent = await getBannerDataContent();
-  // const joinCommunityDataContent = await getJoinCommunityDataContent();
-  // const sugconDataContent = await getSugconDataContent();
-  // const sugconCommunityDataContent = await getSugconCommunityDataContent();
-  // const eventsDataContent = await getEventsDataContent();
+  const navbarDataContent = await getNavbarDataContent();
+  const navigationDataContent = await getNavigationDataContent();
+  const bannerDataContent = await getBannerDataContent();
+  const joinCommunityDataContent = await getJoinCommunityDataContent();
+  const sugconDataContent = await getSugconDataContent();
+  const sugconCommunityDataContent = await getSugconCommunityDataContent();
+  const eventsDataContent = await getEventsDataContent();
 
   return (
     <main className="flex flex-col items-center justify-center pt-0 pb-[20px] px-[20px] lg:pb-[55px] lg:px-[55px] max-w-[1440px] w-full overflow-hidden">
-      {/* <Navbar data={navbarDataContent} />
+      <Navbar data={navbarDataContent} />
       <div className="bg-[#D91E27] w-full h-[5px]"></div>
       <Navigation data={navigationDataContent} />
       <div className="bg-[#D91E27] w-full h-[5px]"></div>
@@ -37,7 +37,7 @@ export default async function Home() {
       <Sugcon data={sugconDataContent} />
       <SugconCommunity data={sugconCommunityDataContent} />
       <div id="events"></div>
-      <Events data={eventsDataContent} /> */}
+      <Events data={eventsDataContent} />
     </main>
   );
 }
